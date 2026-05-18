9ef7e538aa706f9baa6615546121b060b5c53353
Zucaras <edgardojr.ff@gmail.com>
2026-05-18 11:28:11 -0600

    Se agrega test.ts.

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,6 +1,6 @@
-import { standardTypeChecked } from '@vue/eslint-config-standard-with-typescript'
-import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
-import pluginVue from 'eslint-plugin-vue'
+import { standardTypeChecked } from '@vue/eslint-config-standard-with-typescript';
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript';
+import pluginVue from 'eslint-plugin-vue';
 
 export default defineConfigWithVueTs(
   pluginVue.configs['flat/essential'],
---
+++ src/test.ts
@@ -0,0 +1,5 @@
+console.log('Hello, World!')
+
+function teste () { return 25 }
+
+teste()
