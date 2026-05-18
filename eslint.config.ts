7adc31269b0d392e6bdff94a7acd471fc576c094
Zucaras <edgardojr.ff@gmail.com>
2026-05-18 11:01:52 -0600

    Se agrega eslint.config.ts al proyecto.

---
+++ eslint.config.ts
@@ -0,0 +1,10 @@
+import { standardTypeChecked } from '@vue/eslint-config-standard-with-typescript'
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
+import pluginVue from 'eslint-plugin-vue'
+
+export default defineConfigWithVueTs(
+  pluginVue.configs['flat/essential'],
+  vueTsConfigs.recommendedTypeChecked,
+  vueTsConfigs.stylisticTypeChecked,
+  standardTypeChecked
+)
